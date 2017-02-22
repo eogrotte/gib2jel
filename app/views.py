93b5949100e20d35ff5e505217065b866b611a0b
Even Grøtte <eogrotte@stud.ntnu.no>
2017-02-22 20:58:54 +0100

    La til en comment.

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from app import app
 @app.route('/index')
 def index():
     #return "Hello, World!"
+    #Even tester push&commit med å legge inn en kommentar, som har ingen innvirkning på noe som helst annen enn ryddighet.
     user = {'nickname': 'Miguel'}  # fake user
     return render_template('index.html',
                            title='Home',
