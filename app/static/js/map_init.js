49c1967bb5a1a9fa2ffef128cce084b9cc939b78
Lena Seternes <lenaset@gmail.com>
2017-03-05 10:01:11 +0100

    Added bootstrap styling

--- app/static/js/map_init.js
+++ app/static/js/map_init.js
@@ -1,4 +1,5 @@
 var map;
+
 function initMap() {
 	console.log("initMap invoked");
 	
@@ -24,11 +25,82 @@ function initMap() {
 	
 	// Adds layer to map
 	map.addLayer(osmTileLayer);
+	
+	map.addControl(new adminControl());
+	map.addControl(new searchSiteControl());
 }
 
 function showSiteOnMap(name, category, lon, lat) {
 	var marker = L.marker([lon, lat]).addTo(map);
 	marker.bindPopup("<b>" + name + "</b> (" + category + ")").openPopup();
-
-
 }
+
+var adminControl = L.Control.extend({
+ 
+  options: {
+    position: 'topleft' 
+  },
+ 
+  onAdd: function (map) {
+	  console.log("onAdd admin invoked");
+
+    var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
+		console.log("cont added");
+	container.style.width = '250px';
+	container.style.height = '35px';
+
+	container.innerHTML = "<div id='accordionLeft' class='panel-group'>" +
+						"<div class='panel panel-default panel-left'>" +
+							"<div class='panel-heading'>" +
+								"<h4 class='panel-title'>" +
+									"<a data-toggle='collapse' data-parent='#accordionLeft' href='#collapseOne'>Admin</a>" +
+								"</h4>" +
+							"</div>" +
+							"<div id='collapseOne' class='panel-collapse collapse'>" +
+								"<div class='panel-body'>" +
+									"<p>Her kommer administrasjon av steder...</p>" +
+								"</div>" +
+							"</div>" +
+						"</div>";
+ 
+    container.onclick = function(){
+      console.log('buttonClicked');
+    }
+    return container;
+  },
+ 
+});
+
+var searchSiteControl = L.Control.extend({
+ 
+  options: {
+    position: 'topleft' 
+  },
+ 
+  onAdd: function (map) {
+	  console.log("onAdd searchSite invoked");
+    var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
+	container.style.width = '250px';
+	container.style.height = '35px';
+	console.log("cont added");
+	container.innerHTML = "<div id='accordionLeft' class='panel-group'>" +
+						"<div class='panel panel-default panel-left'>" +
+							"<div class='panel-heading'>" +
+								"<h4 class='panel-title'>" +
+									"<a data-toggle='collapse' data-parent='#accordionLeft' href='#collapseTwo'>Søk</a>" +
+								"</h4>" +
+							"</div>" +
+							"<div id='collapseTwo' class='panel-collapse collapse'>" +
+								"<div class='panel-body'>" +
+									"<p>Her kommer søk etter steder...</p>" +
+								"</div>" +
+							"</div>" +
+						"</div>";
+ 
+    container.onclick = function(){
+      console.log('buttonClicked');
+    }
+    return container;
+  },
+ 
+});
