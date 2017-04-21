c0c1f0f48cc2a7b59a7938a182d7a6e90bcfaa78
Lena Seternes <lenaset@gmail.com>
2017-04-21 02:30:49 +0200

    Added remote database

--- app/config.py
+++ app/config.py
@@ -1,7 +1,8 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+#import os
+#basedir = os.path.abspath(os.path.dirname(__file__))
 
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:admin@localhost/gib2db"
+SQLALCHEMY_DATABASE_URI = "postgresql://utvikler:Gib2Pg17@109.237.25.116/gisdb"
+#SQLALCHEMY_DATABASE_URI = "postgresql://postgres:admin@localhost/gib2db"
 #SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
